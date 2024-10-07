175b6876b706159dd4fa07e1e02110bf3a2b7685
Jada Smith <jada.smith@lethpolytech.ca>
2024-10-07 09:05:28 -0600

    wooww!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// get exactly 5 ints from the user and write them to a file called data.txt
+// get exactly 5 ints from the user and write them to a file called data.txt and print the average on screen
 
 #include <iostream>
 #include <fstream>
@@ -7,7 +7,9 @@ using namespace std;
 
 int main() {
 
-	ofstream myDataFile;
+	ofstream myDataFile;			// delcare an obj for the output file
+	ifstream mySourceFile;			// declare obj for the input file
+	double avFromFile = 0.0;		// somewhere to store our average
 
 	myDataFile.open("data.txt");
 
@@ -18,5 +20,20 @@ int main() {
 	}
 
 	myDataFile.close();
+
+	//the average
+	mySourceFile.open("data.txt");
+
+	for (int i = 0; i < 5; i++) {
+		int anInt = 0;
+		mySourceFile >> anInt;
+		myDataFile << anInt << endl;
+		avFromFile += static_cast<double>(anInt);
+	}
+
+	mySourceFile.close();
+
+	avFromFile /= 5.0;
+	cout << "The average from the file is " << avFromFile << endl;
 	return 0;
 }
